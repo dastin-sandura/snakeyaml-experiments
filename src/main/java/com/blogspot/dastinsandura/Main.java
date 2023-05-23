462aebffab387fcceaf48f898d8cb23097dd41d1
Dastin Sandura <d.kuwalek@gmail.com>
2023-05-23 14:58:53 +0200

    Add 3 examples of printing yml
     - added a scenario when snakeyaml prints the content of a yml file in one line (flow style)
     - added a scenario when snakeyaml prints the content of a yml file where each key and value is on separate line (block style)
     - added a scenario when snakeyaml prints the content of a yml with nested collection in block style which is forced by custom 'dumper options'

--- src/main/java/com/blogspot/dastinsandura/Main.java
+++ src/main/java/com/blogspot/dastinsandura/Main.java
@@ -1,7 +1,54 @@
 package com.blogspot.dastinsandura;
 
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("This is a showcase of ho library snakeyaml works.");
+        printExampleOfYamlCollectionWithoutNestedCollections();
+        showExampleOfPrintingYamlCollectionWithNestedCollections();
+        printExampleOfYamlCollectionWithoutNestedCollectionsWithDumperOptions();
+    }
+
+    /**
+     * If the collection has nested collection it will print it in the "block style"
+     * See: <a href="https://redocly.com/docs/yaml/maps/#flow-maps">yaml documentation about flow and block style</a>
+     */
+    private static void showExampleOfPrintingYamlCollectionWithNestedCollections() {
+        Yaml y = new Yaml();
+        String document = " a: 1\n b: \n  x: 7\n  y: 8\n c: 3\n d: 4\n";
+        System.out.println("Source document");
+        System.out.println(document);
+        System.out.println("Document after formatting by snakeyaml");
+        System.out.println(y.dump(y.load(document)));
+    }
+
+    /**
+     * By default, snakeyaml prints collections in the "flow style"
+     * See: <a href="https://redocly.com/docs/yaml/maps/#flow-maps">yaml documentation about flow and block style</a>
+     */
+    private static void printExampleOfYamlCollectionWithoutNestedCollections() {
+        Yaml y = new Yaml();
+        String document = " a: 1\n b:\n c: 3\n d: 4\n";
+        System.out.println("Source document");
+        System.out.println(document);
+        System.out.println("Document after formatting by snakeyaml");
+        System.out.println(y.dump(y.load(document)));
+    }
+
+    /**
+     * By default, snakeyaml prints collections in the "flow style"
+     * See: <a href="https://redocly.com/docs/yaml/maps/#flow-maps">yaml documentation about flow and block style</a>
+     */
+    private static void printExampleOfYamlCollectionWithoutNestedCollectionsWithDumperOptions() {
+        DumperOptions options = new DumperOptions();
+        options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+        Yaml y = new Yaml(options);
+        String document = " a: 1\n b: \n  x: 7\n  y: 8\n c: 3\n d: 4\n";
+        System.out.println("Source document");
+        System.out.println(document);
+        System.out.println("Document after formatting by snakeyaml");
+        System.out.println(y.dump(y.load(document)));
     }
 }
